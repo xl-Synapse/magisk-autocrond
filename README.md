e4ef902880881431e43958522a48fe7f6a68b397
xl-Synapse <498478105@qq.com>
2021-03-02 15:29:03 +0800

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# 自启crond服务
+
+**默认路径为 /data/local/tmp/crontabJob**
+编辑命令：
+    crontab -c /data/local/tmp/crontabJob -e
+
---
+++ service.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+# 不要假设您的模块将位于何处。
+# 如果您需要知道此脚本和模块的放置位置，请使用$MODDIR
+# 这将确保您的模块仍能正常工作
+# 即使Magisk将来更改其挂载点
+MODDIR=${0%/*}
+
+# 此脚本将在late_start service 模式执行
+mkdir /data/local/tmp/crontabJob
+# echo `whoami``date`>>/data/local/tmp/crontabJob/auto.txt
+crond -c /data/local/tmp/crontabJob
+crontab -c /data/local/tmp/crontabJob -u root
